82f018a9f3277f0b353b42c661553df44b23378a
Mihail Solodkov <fantazermyname@gmail.com>
2019-10-05 12:51:03 +0300

    Lab_1
    Version 1.0

---
+++ Lab_1/QualityMetric/QualityMetric/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QualityMetric
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+        #endregion
+        //ввод первой матрицы
+        private void EnterFirstMatrix(object sender, EventArgs e)
+        {
+            
+        }
+        //ввод второй матрицы
+        private void EnterSecondMatrix(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
