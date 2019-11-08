d4198a64fa3a28a706221e9e3fadf638edd09e96
Solodkov Mikhail <fantazermyname@gmail.com>
2019-11-08 16:35:43 +0300

    ghj

--- Lab_2/Salary/SalaryApplocation/Employee.cs
+++ Lab_2/Salary/SalaryApplocation/Employee.cs
@@ -6,17 +6,17 @@ using System.Threading.Tasks;
 
 namespace SalaryApplocation
 {
-    public class EmployeeSalary
+    public class Employee
     {
-        public string name { get; set; }
-        public string lastName { get; set; }
-        public string secondName { get; set; }
+        public string name { get; set; } //имя сотрудника
+        public string lastName { get; set; } //фамилия сотрудника
+        public string secondName { get; set; } //отчество сотрудника
 
         public double employeeWageCategory { get; private set; } //тарифная категория работника.
         public double scopeOfWork { get; private set; } //объем выполненной работы (в процентах).
         public double unitCost { get; private set; } //стоимость единицы продукции.
 
-        public EmployeeSalary(string name, string lastName, string secondName, double employeeWageCategory, double scopeOfWork
+        public Employee(string name, string lastName, string secondName, double employeeWageCategory, double scopeOfWork
                               , double unitCost)
         {
             this.name = name;
@@ -26,12 +26,8 @@ namespace SalaryApplocation
             this.scopeOfWork = scopeOfWork;
             this.unitCost = unitCost;
         }
-
-        public EmployeeSalary()
-        {
-
-        }
-        public double EmployeeWageCategory
+        public Employee(){}
+        public double EmployeeWageCategory //сеттер для тарифной категории работника
         {
             set
             {
@@ -43,7 +39,7 @@ namespace SalaryApplocation
             }
         }
 
-        public double ScopeOfWork
+        public double ScopeOfWork //сеттер для объема выполненной работы
         {
             set
             {
@@ -55,7 +51,7 @@ namespace SalaryApplocation
             }
         }
 
-        public double UnitCost
+        public double UnitCost //сеттер для стоимости единицы продукции
         {
             set
             {
@@ -71,6 +67,33 @@ namespace SalaryApplocation
         {
             return "Name: " + name + "\nLast Name: " + lastName + "\nSecond Name: " + secondName + "\nEmployee Wage Category: " +
                 +employeeWageCategory + "\nScope of Work: " + scopeOfWork + "\nUnit Cost: " + unitCost + ".";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (this == obj){
+                return true;
+            }
+            if (obj == null || this.getClass != obj.getClass){
+                return false;
+            }
+            Employee employee = (Employee) obj;
+            if(this.name == employee.name || this.lastName == employee.lastName || this.secondName == employee.secondName || this.employeeWageCategory == employee.employeeWageCategory ||
+                this.scopeOfWork == employee.scopeOfWork || this.unitCost == employee.unitCost){
+                return true;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            int result = 4;
+            result = 12 * result + name.Length;
+            result = 12 * result + lastName.Length;
+            result = 12 * result + secondName.Length;
+            result = 12 * result + (int)employeeWageCategory;
+            result = 12 * result + (int)scopeOfWork;
+            result = 12 * result + (int)unitCost;
+            return result;
         }
     }
 }
